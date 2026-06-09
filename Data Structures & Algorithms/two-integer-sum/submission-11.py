7b225d25b5fed50fe9ae141f43ee22aec0ab4074
Rasika Yeolekar <yeolekar.r@northeastern.edu>
2026-06-09 16:00:45 -0700

    Add: two-integer-sum - submission-11

---
+++ Data Structures & Algorithms/two-integer-sum/submission-11.py
@@ -0,0 +1,17 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        my_map = {}
+
+        for i in range(len(nums)):
+            diff = target - nums[i]
+
+            if diff in my_map:
+                return [my_map[diff], i]
+            else:
+                my_map[nums[i]] = i
+
+#TC: O(n)
+#SC: O(n)
+        
+
+        
