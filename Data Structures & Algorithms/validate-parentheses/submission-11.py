6c1d0947bb09c13c995a3d1781266344529537d4
Rasika Yeolekar <yeolekar.r@northeastern.edu>
2026-07-16 13:50:08 -0700

    Add: validate-parentheses - submission-11

---
+++ Data Structures & Algorithms/validate-parentheses/submission-11.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        my_stack = []
+        my_map = {']':'[', ')':'(', '}':'{'}
+
+        for p in s:
+            if p in my_map:
+                if my_stack and my_stack[-1] == my_map[p]:
+                    my_stack.pop()
+                else:
+                    return False
+            else:
+                my_stack.append(p)
+        
+        return True if not my_stack else False
+
+                
+        
